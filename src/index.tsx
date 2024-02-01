d66b91e7e556d9db1275e0980e2b8f66c009ce67
Shudeliang <1023756490@qq.com>
2024-02-01 08:59:04 +0800

    modify webpack.config.js

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@ root.render(
     <>
         <StrictMode>
             <div style={{height: '100vh', width: '100vw', flexFlow: 'column', gap: 20, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                {/*<Example1/>*/}
-                <ExampleMultiple/>
+                <Example1/>
+                {/*<ExampleMultiple/>*/}
             </div>
         </StrictMode>
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,27 @@ const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const APP_PATH = path.resolve(__dirname, 'src');
+const {merge} = require('webpack-merge');
 
 module.exports = (env) => {
-    console.log("=>(webpack.config.js:9) env", env);
     const isDev = env.mode === 'development';
-    return {
-        mode: isDev ? 'development' : 'production',
-        entry: isDev ? APP_PATH : path.join(APP_PATH, './bridge.tsx'),
+
+    console.log("=>(webpack.config.js:9) isDev", isDev);
+
+    /**
+     *
+     * @type {import('webpack').Configuartion}
+     */
+    const commonConfig = {
         output: {
+            clean: true,
             path: path.resolve(__dirname, 'dist'),
-            filename: 'app.bundle.js'
+            filename: 'bridge.js',
+            library:{
+                name: 'MyLibrary',
+                type: 'umd',
+            }
+
         },
         resolve: {
             extensions: ['.ts', '.tsx', '.js', '.json']
@@ -23,16 +34,42 @@ module.exports = (env) => {
                 loader: 'babel-loader',
             }],
         },
-        performance: {
-            hints: false
-        },
         plugins: [
-            new HtmlWebpackPlugin({ inject: true, template: path.join(APP_PATH, 'index.html') }),
             new ForkTsCheckerWebpackPlugin(),
         ],
-        devtool: 'eval-source-map',
-        devServer: {
-            open: true,
+        performance: {
+            hints: false
         }
     }
+
+
+    return merge(commonConfig, isDev ? devConfig : proConfig);
 };
+
+/**
+ *
+ * @type {import('webpack').Configuartion}
+ */
+const devConfig = {
+    mode: 'development',
+    entry: APP_PATH,
+    plugins: [
+        new HtmlWebpackPlugin({ inject: true, template: path.join(APP_PATH, 'index.html') }),
+    ],
+    devtool: 'eval-source-map',
+    devServer: {
+        open: true,
+    }
+};
+
+/**
+ *
+ * @type {import('webpack').Configuartion}
+ */
+const proConfig = {
+    mode: 'production',
+    entry: path.join(APP_PATH, './bridge.tsx'),
+    externals: {
+        react: 'umd react',
+    }
+}
