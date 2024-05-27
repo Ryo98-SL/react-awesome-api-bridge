38e5778646a673bf080b8a5a65b7fb071d36d3c6
shudeliang <shudeliang@metasota.ai>
2024-05-27 12:50:07 +0800

    configuration update

--- test/example1.test.tsx
+++ test/example1.test.tsx
@@ -7,21 +7,21 @@ import Example1 from "../web-server-dev/examples/example1";
 test(`test example 1`,async  () => {
     render(<Example1 />)
 
-    await fireEvent.click(screen.getByText(`callA without hook`));
+    fireEvent.click(screen.getByText(`callA without hook`));
     expect(screen.getByRole(`alert`)).toHaveTextContent(/from:outer$/);
 
-    await fireEvent.click(screen.getByText(`Indirectly invoke "bark" of A by invoking "invokeA" of B`));
+    fireEvent.click(screen.getByText(`Indirectly invoke "bark" of A by invoking "invokeA" of B`));
     expect(screen.getByRole(`alert`)).toHaveTextContent(/from:outer$/);
 
 
-    await fireEvent.click(screen.getByText(`toggle B`));
+    fireEvent.click(screen.getByText(`toggle B`));
     expect(screen.getByRole(`alert`)).toHaveTextContent(/from:Root$/);
-    await fireEvent.click(screen.getByText(`Indirectly invoke "bark" of A by invoking "invokeA" of B`));
+    fireEvent.click(screen.getByText(`Indirectly invoke "bark" of A by invoking "invokeA" of B`));
     expect(screen.getByRole(`alert`)).toHaveTextContent(/from:indirect$/);
-    await fireEvent.click(screen.getByText(`invoke A's bark`));
+    fireEvent.click(screen.getByText(`invoke A's bark`));
     expect(screen.getByRole(`alert`)).toHaveTextContent(/from:B$/);
 
-    await fireEvent.click(screen.getByText(`callA without hook`));
+    fireEvent.click(screen.getByText(`callA without hook`));
 
     expect(screen.getByRole(`alert`)).toHaveTextContent(/from:outer$/);
 })
